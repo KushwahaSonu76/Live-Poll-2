3ef38166fd70efe0b28b6ff60671405f353391ce
ritesh kushwaha <sonukushwaha821304@gmail.com>
2026-07-03 22:41:10 +0530

    Configure Buffer and global polyfills in main.tsx entrypoint

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { Buffer } from 'buffer';
+
+(window as any).Buffer = Buffer;
+(window as any).global = window;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
